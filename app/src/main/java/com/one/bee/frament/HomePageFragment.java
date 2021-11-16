de0d7e3befa6afbedaa9af1b461377e66fd0a2c6
onebee <diaokaibin@gmail.com>
2021-11-16 21:37:17 +0800

    update HiTabTopLayout

--- app/src/main/java/com/one/bee/frament/HomePageFragment.java
+++ app/src/main/java/com/one/bee/frament/HomePageFragment.java
@@ -1,15 +1,65 @@
 package com.one.bee.frament;
 
+import android.widget.Toast;
+
 import com.one.bee.R;
 import com.one.common.ui.component.HiBaseFragment;
+import com.one.ui.tab.common.IHiTabLayout;
+import com.one.ui.tab.top.HiTabTopInfo;
+import com.one.ui.tab.top.HiTabTopLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 /**
  * @author diaokaibin@gmail.com on 2021/11/15.
  */
 public class HomePageFragment extends HiBaseFragment {
+    String[] tabsStr = new String[]{
+            "热门",
+            "服装",
+            "数码",
+            "鞋子",
+            "零食",
+            "家电",
+            "汽车",
+            "百货",
+            "家居",
+            "装修",
+            "运动"
+    };
 
     @Override
     public int getLayoutId() {
         return R.layout.fragment_home;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        initTabTop();
+    }
+
+    private void initTabTop() {
+        HiTabTopLayout hiTabTopLayout = getActivity().findViewById(R.id.tab_top_layout);
+        List<HiTabTopInfo<?>> infoList = new ArrayList<>();
+        int defaultColor = getResources().getColor(R.color.tabBottomDefaultColor);
+        int tintColor = getResources().getColor(R.color.tabBottomTintColor);
+        for (String s : tabsStr) {
+            HiTabTopInfo<?> info = new HiTabTopInfo<Integer>(s, defaultColor, tintColor);
+            infoList.add(info);
+        }
+        hiTabTopLayout.inflateInfo(infoList);
+        hiTabTopLayout.addTabSelectedChangeListener(new IHiTabLayout.OnTabSelectedListener<HiTabTopInfo<?>>() {
+            @Override
+            public void onTabSelectedChange(int index, @Nullable HiTabTopInfo<?> prevInfo, @NonNull HiTabTopInfo<?> nextInfo) {
+                Toast.makeText(getActivity(), nextInfo.name, Toast.LENGTH_SHORT).show();
+            }
+        });
+        hiTabTopLayout.defaultSelected(infoList.get(0));
     }
 }
--- hi-ui/src/main/java/com/one/ui/tab/top/HiTabTopLayout.java
+++ hi-ui/src/main/java/com/one/ui/tab/top/HiTabTopLayout.java
@@ -1,11 +1,13 @@
 package com.one.ui.tab.top;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 
+import com.one.library.util.HiDisplayUtil;
 import com.one.ui.tab.common.IHiTabLayout;
 
 import org.jetbrains.annotations.NotNull;
@@ -19,23 +21,25 @@ import androidx.annotation.NonNull;
 /**
  * @author diaokaibin@gmail.com on 2021/11/16.
  */
-public class HiTapTopLayout extends HorizontalScrollView implements IHiTabLayout<HiTabTop, HiTabTopInfo<?>> {
+public class HiTabTopLayout extends HorizontalScrollView implements IHiTabLayout<HiTabTop, HiTabTopInfo<?>> {
 
     private List<OnTabSelectedListener<HiTabTopInfo<?>>> tabSelectedListeners = new ArrayList<>();
     private HiTabTopInfo<?> selectedInfo;
     private List<HiTabTopInfo<?>> infoList;
 
 
-    public HiTapTopLayout(Context context) {
+    public HiTabTopLayout(Context context) {
         this(context, null);
     }
 
-    public HiTapTopLayout(Context context, AttributeSet attrs) {
+    public HiTabTopLayout(Context context, AttributeSet attrs) {
         super(context, attrs, 0);
     }
 
-    public HiTapTopLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+    public HiTabTopLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setHorizontalScrollBarEnabled(false);
+
     }
 
     @Override
@@ -107,6 +111,93 @@ public class HiTapTopLayout extends HorizontalScrollView implements IHiTabLayout
             listener.onTabSelectedChange(infoList.indexOf(nextInfo), selectedInfo, nextInfo);
         }
         this.selectedInfo = nextInfo;
+        autoScroll(nextInfo);
+    }
+
+    int tabWith;
+
+    private void autoScroll(HiTabTopInfo<?> info) {
+
+        HiTabTop tab = findTab(info);
+        if (tab == null) return;
+        int index = infoList.indexOf(info);
+        int[] loc = new int[2];
+        //获取点击的控件在屏幕中的位置
+        tab.getLocationInWindow(loc);
+
+        int scrollWidth;
+        if (tabWith == 0) {
+            tabWith = tab.getWidth();
+        }
+        // 判断点击了屏幕左边还是右侧
+        if ((loc[0] + tabWith / 2) > HiDisplayUtil.getDisplayWidthInPx(getContext()) / 2) {
+            scrollWidth = rangeScrollWidth(index, 2);
+        } else {
+            scrollWidth = rangeScrollWidth(index, -2);
+        }
+        scrollTo(getScrollX() + scrollWidth,0);
+    }
+
+    /**
+     * 获取可滚动的范围
+     *
+     * @param index 从第几个开始
+     * @param range 向前向后的范围
+     * @return 可滚动的范围
+     */
+    private int rangeScrollWidth(int index, int range) {
+        int scrollWidth = 0;
+        for (int i = 0; i <= Math.abs(range); i++) {
+            int next;
+            if (range < 0) {
+                next = range + i + index;
+            } else {
+                next = range - i + index;
+            }
+            if (next >= 0 && next < infoList.size()) {
+                if (range < 0) {
+                    scrollWidth -= scrollWidth(next, false);
+                } else {
+                    scrollWidth += scrollWidth(next, true);
+                }
+            }
+        }
+        return scrollWidth;
+
+    }
+
+
+    /***
+     * 指定位置的控件可滚动的距离
+     * @param index 指定位置的控件
+     * @param toRight 是否点击了屏幕右侧
+     * @return 可滚动的距离
+     */
+    private int scrollWidth(int index, boolean toRight) {
+        HiTabTop target = findTab(infoList.get(index));
+        if (target == null) return 0;
+        Rect rect = new Rect();
+        target.getLocalVisibleRect(rect);
+        if (toRight) {
+            if (rect.right > tabWith) {
+                // right 坐标大于控件的宽度,说明完全没有显示
+                return tabWith;
+            } else {
+                // 显示部分,减去已显示的宽度
+                return tabWith - rect.right;
+
+            }
+        } else {
+            if (rect.left <= -tabWith) {
+                // left 坐标小于等于控件宽度,说明完全没有显示
+                return tabWith;
+            } else if (rect.left > 0) {
+                // 显示部分
+                return rect.left;
+            }
+        }
+        return 0;
+
     }
 
     private LinearLayout getRootLayout(boolean clear) {
