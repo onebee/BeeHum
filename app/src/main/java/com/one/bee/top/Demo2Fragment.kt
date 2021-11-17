6ec164ffc43703e6ec284eacdda21654470c8376
onebee <diaokaibin@gmail.com>
2021-11-17 10:47:50 +0800

    update sample

---
+++ app/src/main/java/com/one/bee/top/Demo2Fragment.kt
@@ -0,0 +1,23 @@
+package com.one.bee.top
+
+import com.one.bee.R
+import com.one.common.ui.component.HiBaseFragment
+import kotlinx.android.synthetic.main.demo1_fragment.*
+
+/**
+ * @author  diaokaibin@gmail.com on 2021/11/17.
+ */
+class Demo2Fragment : HiBaseFragment() {
+    
+
+    override fun getLayoutId(): Int {
+        return R.layout.demo1_fragment
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+
+        tv.text= "服装"
+    }
+}
