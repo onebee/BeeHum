d4ea21689b92f1040f50e832f8c6f94b0d40eb76
onebee <diaokaibin@gmail.com>
2021-11-17 11:39:08 +0800

    update sample

--- app/src/main/java/com/one/bee/frament/HomePageFragment.java
+++ app/src/main/java/com/one/bee/frament/HomePageFragment.java
@@ -1,8 +1,20 @@
 package com.one.bee.frament;
 
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.one.bee.R;
+import com.one.bee.top.Demo10Fragment;
+import com.one.bee.top.Demo11Fragment;
+import com.one.bee.top.Demo1Fragment;
+import com.one.bee.top.Demo2Fragment;
+import com.one.bee.top.Demo3Fragment;
+import com.one.bee.top.Demo4Fragment;
+import com.one.bee.top.Demo5Fragment;
+import com.one.bee.top.Demo6Fragment;
+import com.one.bee.top.Demo7Fragment;
+import com.one.bee.top.Demo8Fragment;
+import com.one.bee.top.Demo9Fragment;
 import com.one.common.ui.component.HiBaseFragment;
 import com.one.ui.tab.common.IHiTabLayout;
 import com.one.ui.tab.top.HiTabTopInfo;
@@ -13,6 +25,7 @@ import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 /**
  * @author diaokaibin@gmail.com on 2021/11/15.
@@ -32,6 +45,20 @@ public class HomePageFragment extends HiBaseFragment {
             "运动"
     };
 
+    Class[] fs = new Class[]{
+            Demo1Fragment.class,
+            Demo2Fragment.class,
+            Demo3Fragment.class,
+            Demo4Fragment.class,
+            Demo5Fragment.class,
+            Demo6Fragment.class,
+            Demo7Fragment.class,
+            Demo8Fragment.class,
+            Demo9Fragment.class,
+            Demo10Fragment.class,
+            Demo11Fragment.class,
+    };
+
     @Override
     public int getLayoutId() {
         return R.layout.fragment_home;
@@ -46,20 +73,44 @@ public class HomePageFragment extends HiBaseFragment {
 
     private void initTabTop() {
         HiTabTopLayout hiTabTopLayout = getActivity().findViewById(R.id.tab_top_layout);
+        FrameLayout fl = getActivity().findViewById(R.id.ll_container);
         List<HiTabTopInfo<?>> infoList = new ArrayList<>();
         int defaultColor = getResources().getColor(R.color.tabBottomDefaultColor);
         int tintColor = getResources().getColor(R.color.tabBottomTintColor);
-        for (String s : tabsStr) {
+        for (int i = 0; i < tabsStr.length; i++) {
+
+            String s = tabsStr[i];
             HiTabTopInfo<?> info = new HiTabTopInfo<Integer>(s, defaultColor, tintColor);
             infoList.add(info);
+
+            info.fragment = fs[i];
         }
         hiTabTopLayout.inflateInfo(infoList);
         hiTabTopLayout.addTabSelectedChangeListener(new IHiTabLayout.OnTabSelectedListener<HiTabTopInfo<?>>() {
             @Override
             public void onTabSelectedChange(int index, @Nullable HiTabTopInfo<?> prevInfo, @NonNull HiTabTopInfo<?> nextInfo) {
                 Toast.makeText(getActivity(), nextInfo.name, Toast.LENGTH_SHORT).show();
+
+                try {
+                    Fragment fragment = nextInfo.fragment.newInstance();
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(fl.getId(), fragment, nextInfo.name).commitAllowingStateLoss();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (java.lang.InstantiationException e) {
+                    e.printStackTrace();
+                }
             }
         });
         hiTabTopLayout.defaultSelected(infoList.get(0));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    private void initFragmentTabView() {
+
     }
 }
